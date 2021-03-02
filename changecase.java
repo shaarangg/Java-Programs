ce74c8f5197ba3a70ec17961b675a2325d65ca03
shaarangg <shaaranggsingh@gmail.com>
2021-03-02 20:08:06 +0530

    feat: Did something

--- changecase.java
+++ changecase.java
@@ -4,18 +4,17 @@ public class changecase {
         Scanner sc = new Scanner(System.in);
         String s = sc.nextLine();
         int a;
-        String s1="";
+        StringBuffer s1 = new StringBuffer();
         for(int i =0; i<s.length(); i++){
             a = s.charAt(i);
             if(a>=65 && a<=90){
-                s1+=(char)(a+32);
+                s1.append((char)(a+32));
             }
             else{
-                s1+=(char)(a-32);
+                s1.append((char)(a-32));
             }
         }
         System.out.println(s1);
         sc.close();
     }
-    
 }
--- palindromestring.java
+++ palindromestring.java
@@ -1,15 +1,16 @@
 import java.util.Scanner;
 public class palindromestring {
     public static void main(String[] args) {
+        StringBuffer s1 = new StringBuffer();
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a string");
         String s = sc.nextLine();
         int l = s.length();
-        String s1="";
         for(int i =l-1; i>=0; i--){
-            s1+=s.charAt(i);
+            s1.append(s.charAt(i));
         }
-        if(s.equals(s1)==true){
+        String s2 = s1.toString();
+        if(s.equals(s2)){
             System.out.println("Palindrome");
         }
         else{

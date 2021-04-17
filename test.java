5d17e69f1cf7e4e67b69757537b85bfab394887c
shaarangg <shaaranggsingh@gmail.com>
2021-04-17 19:43:41 +0530

    feat: Fixed the order and stuff

--- test.java
+++
@@ -1,9 +0,0 @@
-public class test {
-    void display(){
-        System.out.println("Hello World!");
-    }
-    public static void main(String[] args){
-        test obj = new test();
-        obj.display();
-    }
-}
